97f214d1a01a241a5a93982e21d11505e68bfb7c
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2019-01-17 11:04:44 +0300

    Changed the rust build script to use the new branch

--- setup_cardano_crypto.sh
+++ setup_cardano_crypto.sh
@@ -2,10 +2,8 @@
 
 git submodule update --init --recursive && \
 cd js-cardano-wasm && \
-git checkout def5d9a0ebd69435d29e737c7e99ea04f692ab70 && \
-cd rust && \
-git checkout 8b441cbbeba32ada20d065399083a16d85a5fe29 && \
-cd .. && \
+git checkout wasm-pack-version && \
+git submodule update && \
 npm install && \
 ../js-cardano-wasm-build && \
 npm link && \
