dd2e048840b7bd012d3d14da37f700a6353b33db
Roberto C. Morano <rcmova@gmail.com>
2019-05-27 12:01:05 +0200

    testing

--- .travis/build_yoroi.sh
+++ .travis/build_yoroi.sh
@@ -4,6 +4,7 @@ set -xeo pipefail
 
 if [ ! -e "build/${GIT_SHORT_COMMIT}.built" ]
 then
+  rm -f artifacts/*
   npm run test-prepare
   mv *crx *xpi artifacts/
   touch build/${GIT_SHORT_COMMIT}.built
--- .travis/e2e_tests.sh
+++ .travis/e2e_tests.sh
@@ -2,6 +2,7 @@
 
 set -xeo pipefail    
 
+npm install # everything is already there but binaries (cucumber-js) are not in context
 cp -a artifacts/*crx .
 cp -a artifacts/*xpi .
 npm run test-e2e-${BROWSER}
