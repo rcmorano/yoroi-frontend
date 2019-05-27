1141585dcdd9592e7bf79137859f7358c7e3ab16
Roberto C. Morano <rcmova@gmail.com>
2019-05-27 12:37:49 +0200

    testing

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,7 +2,6 @@
 
 set -xeo pipefail
 
-/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- ${DISPLAY} -ac -screen 0 ${SCREEN_RESOLUTION}
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
 if [ ! -e "$HOME/.cargo/bin" ]
--- .travis/e2e_tests.sh
+++ .travis/e2e_tests.sh
@@ -2,7 +2,8 @@
 
 set -xeo pipefail    
 
-npm install # everything is already there but binaries (cucumber-js) are not in context
+PATH=$PATH:$(npm bin)
+
 cp -a artifacts/*crx .
 cp -a artifacts/*xpi .
 npm run test-e2e-${BROWSER}
