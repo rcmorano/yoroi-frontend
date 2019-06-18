5aff2f099fcf233c5690879f6f53095e11f53c3e
Roberto C. Morano <rcmova@gmail.com>
2019-06-18 12:42:06 +0200

    testing

--- .circleci/before_install.sh
+++ .circleci/before_install.sh
@@ -2,10 +2,23 @@
 
 set -xeo pipefail
 
+# Because npm link will write in this path
+sudo chown -R "$(whoami):$(whoami)" /usr/local/lib/node_modules
+
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 echo 'export PATH=$HOME/.cargo/bin/:$PATH' >> ${BASH_ENV}
 source $BASH_ENV
-rustup install ${RUST_VERSION}
-rustup target add ${RUST_TARGETS} --toolchain ${RUST_VERSION}
+if [ -z "$(ls ~/.rustup | grep ^${RUST_VERSION}-)" ]
+then
+  rustup install ${RUST_VERSION}
+fi
+
+for rust_target in ${RUST_TARGETS}
+do
+  if [ ! -e "~/.rustup/toolchains/${RUST_VERSION}*/lib/rustlib/manifest-rust-std-${rust_target}" ]
+  then
+    rustup target add ${RUST_TARGETS} --toolchain ${RUST_VERSION}
+  fi
+done
 
 sudo apt-get install -qq -y python-pip && sudo pip install awscli
