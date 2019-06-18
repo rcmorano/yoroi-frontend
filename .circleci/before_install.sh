6b8dcf630cbf1d650734dea648ed148fce763b56
Roberto C. Morano <rcmova@gmail.com>
2019-06-18 13:37:35 +0200

    testing

--- .circleci/before_install.sh
+++ .circleci/before_install.sh
@@ -5,9 +5,14 @@ set -xeo pipefail
 # Because npm link will write in this path
 sudo chown -R "$(whoami):$(whoami)" /usr/local/lib/node_modules
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-echo 'export PATH=$HOME/.cargo/bin/:$PATH' >> ${BASH_ENV}
+if [ -z "$(which cargo)" ]
+then
+  curl https://sh.rustup.rs -sSf | sh -s -- -y
+  echo 'export PATH=$HOME/.cargo/bin/:$PATH' >> ${BASH_ENV}
+fi
+
 source $BASH_ENV
+
 if [ -z "$(ls ~/.rustup/toolchains | grep ^${RUST_VERSION}-)" ]
 then
   rustup install ${RUST_VERSION}
@@ -15,7 +20,7 @@ fi
 
 for rust_target in ${RUST_TARGETS}
 do
-  if [ ! -e "~/.rustup/toolchains/${RUST_VERSION}*/lib/rustlib/manifest-rust-std-${rust_target}" ]
+  if [ ! -e ~/.rustup/toolchains/${RUST_VERSION}*/lib/rustlib/manifest-rust-std-${rust_target} ]
   then
     rustup target add ${RUST_TARGETS} --toolchain ${RUST_VERSION}
   fi
--- .circleci/build_yoroi.sh
+++ .circleci/build_yoroi.sh
@@ -5,6 +5,7 @@ set -xeo pipefail
 if [ ! -e "build/${GIT_SHORT_COMMIT}.built" ]
 then
   rm -f artifacts/*
+  mkdir -p artifacts
   npm run test-prepare
   mv *crx *xpi artifacts/
   touch build/${GIT_SHORT_COMMIT}.built
