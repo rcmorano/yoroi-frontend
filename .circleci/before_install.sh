b379b0f0f75d5d879a4673ed39c24e5a6c5f9dbd
Roberto C. Morano <rcmova@gmail.com>
2019-06-18 11:56:49 +0200

    testing

--- .circleci/before_install.sh
+++ .circleci/before_install.sh
@@ -3,7 +3,8 @@
 set -xeo pipefail
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
-echo 'export PATH=$HOME/.cargo/bin/:$PATH' >> ~/.bashrc
+echo 'export PATH=$HOME/.cargo/bin/:$PATH' >> ${BASH_ENV}
+source $BASH_ENV
 rustup install ${RUST_VERSION}
 rustup target add ${RUST_TARGETS} --toolchain ${RUST_VERSION}
 
