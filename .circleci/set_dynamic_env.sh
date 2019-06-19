d98a3b5b75f686ea0f462469183328ce9abaff2e
Roberto C. Morano <rcmova@gmail.com>
2019-06-19 13:14:46 +0200

    testing

--- .circleci/set_dynamic_env.sh
+++ .circleci/set_dynamic_env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo -e "export GIT_SHORT_COMMIT: ${CIRCLE_SHA1:0:7}" >> $BASH_ENV
 echo -e "export CIRCLE_PR_NUMBER=$(bash .circleci/get_github_pr_for_associated_branch.sh)" >> $BASH_ENV
 echo -e "export CIRCLE_PR_BASE_BRANCH=$(bash .circleci/get_github_pr_base_branch.sh)" >> $BASH_ENV
