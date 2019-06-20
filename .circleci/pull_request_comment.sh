a37ade5a0a445c7f6e0e6262c2637b8827ded4b3
Roberto C. Morano <rcmova@gmail.com>
2019-06-20 12:05:10 +0200

    testing

--- .circleci/pull_request_comment.sh
+++ .circleci/pull_request_comment.sh
@@ -22,11 +22,7 @@ then
  
   for browser in brave chrome firefox
   do
-    # comment header
-  cat > /tmp/${browser}-pr-comment.json <<EOF
-{ "body": "
-EOF
-  
+ 
     OBJECT_KEY_BASEPATH="screenshots/${browser}/${PR_NUMBER}-${GIT_SHORT_COMMIT}"
   
     set +e; aws s3 cp "s3://${S3_BUCKET}/${OBJECT_KEY_BASEPATH}/pr-screenshots-urls" /tmp/${browser}-pr-screenshots-urls; set -e
@@ -34,36 +30,37 @@ EOF
     # add differences detail only if we found any
     if [ -e /tmp/${browser}-pr-differences-urls ]
     then
-      cat >> /tmp/${browser}-pr-comment.json <<EOF
+      cat >> /tmp/${browser}-pr-differences-comment.json <<EOF
+{ "body": "
 <details>\n
   <summary>E2E _${browser}_ screenshots differences between 'PR${PR_NUMBER}-${GIT_SHORT_COMMIT}' and base branch '${TRAVIS_BRANCH}'</summary>\n\n
 $(cat /tmp/${browser}-pr-differences-urls | while read line; do echo "\\n\\n  $line\\n\\n"; done)\n\n
 </details>\n
+"}
 EOF
     fi
   
     if [ -e /tmp/${browser}-pr-screenshots-urls ]
     then
-      cat >> /tmp/${browser}-pr-comment.json <<EOF
+      cat >> /tmp/${browser}-pr-collection-comment.json <<EOF
+{ "body": "
 <details>\n
   <summary>Complete E2E _${browser}_ screenshots collection for 'PR${PR_NUMBER}-${GIT_SHORT_COMMIT}'</summary>\n\n
 $(cat /tmp/${browser}-pr-screenshots-urls | while read line; do echo "\\n\\n  $line\\n\\n"; done)\n\n
 </details>\n
+"}
 EOF
     fi
 
     # check if there is something to comment
-    if [ $(cat /tmp/${browser}-pr-comment.json | wc -l) -gt 2 ]
+    for file in /tmp/${browser}-pr-collection-comment.json /tmp/${browser}-pr-differences-comment.json
+    if [ $(cat ${file} | wc -l) -gt 2 ]
     then
-      cat >> /tmp/${browser}-pr-comment.json <<EOF
-"}
-EOF
-      set +e; aws s3 cp /tmp/${browser}-pr-comment.json "s3://${S3_BUCKET}/${OBJECT_KEY_BASEPATH}/${browser}-pr-comment.json"; set -e
+      set +e; aws s3 cp "${file}" "s3://${S3_BUCKET}/${OBJECT_KEY_BASEPATH}/$(basename "${file}")"; set -e
       curl -s -H "Authorization: token ${GITHUB_PAT}" \
-        -X POST --data @/tmp/${browser}-pr-comment.json \
+        -X POST --data @${file} \
         "https://api.github.com/repos/${REPO_SLUG}/issues/${PR_NUMBER}/comments"
     fi
-
 
   done
 
